6a6b1f5d3a0de39d5f288d4c3022d4ebbab2a242
G4N P <53969490+xG4nP@users.noreply.github.com>
2025-11-27 15:49:47 +0800

    Update clkdbg.c

--- drivers/clk/mediatek/clkdbg.c
+++ drivers/clk/mediatek/clkdbg.c
@@ -1143,7 +1143,7 @@ static int (struct seq_file *s, void *v)
 	return 0;
 }
 
-static int clkdbg_reg_set(struct seq_file *s, void *v)
+static int (struct seq_file *s, void *v)
 {
 	void __iomem *reg;
 	unsigned long val = 0;

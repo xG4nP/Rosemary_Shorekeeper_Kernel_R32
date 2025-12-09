480b7df24546e53ac40694fe32090f84fff7a38e
G4N P <53969490+xG4nP@users.noreply.github.com>
2025-12-09 14:09:08 +0800

    Update: core

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4502,7 +4502,7 @@ static noinline void __schedule_bug(struct task_struct *prev)
 {
 	/* Save this before calling printk(), since that will clobber it */
 	unsigned long preempt_disable_ip = get_preempt_disable_ip(current);
-	int = 0;
+	int i = 0;
 	if (oops_in_progress)
 		return;
 
